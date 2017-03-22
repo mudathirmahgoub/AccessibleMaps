f53efb4257cb7422b75c0c421a6ac77d7c012009
mahgoubyahia <mudathirmahgoub@gmail.com>
2017-03-22 17:20:11 -0500

    some progress

--- contents.js
+++ contents.js
@@ -1,4 +1,25 @@
-chrome.runtime.sendMessage({action:"show"});
+$(function () {
+    // enable the page extension
+    chrome.runtime.sendMessage({action: "show"});
+
+    // listen to messages from the extension
+    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+
+        // if the message type is filter
+        if (request.type == "filter") {
+            console.log("Inversion :" + request.inversionEnabled);
+            if(request.inversionEnabled) {
+                // invert the colors
+                $("body").addClass("inversionEnabled");
+            }
+            else
+            {
+                // remove the inversion
+                $("body").removeClass("inversionEnabled");
+            }
+        }
+    });
+});
 
 
 // get the contents of the dom
@@ -6,3 +27,5 @@ chrome.runtime.sendMessage({action:"show"});
 // determine the color-contrast between adjacent elements
 // if the color-contrast is good, we are done
 // otherwise, choose high-contrasted colors.
+
+
--- popup.js
+++ popup.js
@@ -1,13 +1,12 @@
-
 $(function () {
 
     // variable to enable or disable accessible maps
-    var enabled  = false;
+    var enabled = false;
     // enable button element
     var enableButton = $("#enableButton");
 
     // read the last value for enabled from the storage
-    chrome.storage.sync.get(['enabled'], function (items){
+    chrome.storage.sync.get(['enabled'], function (items) {
         enabled = (items.enabled == 'true');
     });
 
@@ -24,9 +23,8 @@ $(function () {
     chrome.pageAction.onClicked.addListener(toggleEnabledCommand);
 
     // add listener for the the toggle command
-    chrome.commands.onCommand.addListener(function(command)
-    {
-        if(command == "toggleColorsInversion"){
+    chrome.commands.onCommand.addListener(function (command) {
+        if (command == "toggleColorsInversion") {
             toggleEnabledCommand();
         }
     });
@@ -45,14 +43,12 @@ $(function () {
             chrome.tts.speak('Disabled');
         }
 
-        chrome.tabs.getSelected(null, function(tab){
-            if(enabled)
-            {
-                chrome.pageAction.setIcon({tabId: tab.id,path: { "128": "images/enabledIcon.png"}});
+        chrome.tabs.getSelected(null, function (tab) {
+            if (enabled) {
+                chrome.pageAction.setIcon({tabId: tab.id, path: {"128": "images/enabledIcon.png"}});
             }
-            else
-            {
-                chrome.pageAction.setIcon({tabId: tab.id,path: { "128": "images/disabledIcon.png"}});
+            else {
+                chrome.pageAction.setIcon({tabId: tab.id, path: {"128": "images/disabledIcon.png"}});
             }
         });
         console.log("enabled = " + enabled);
@@ -60,9 +56,18 @@ $(function () {
 
     // function to toggle enabled variable
     function toggleEnabledCommand() {
+        // reverse the boolean enabled
         enabled = !enabled;
+        // store the new value in the storage
         chrome.storage.sync.set({'enabled': enabled});
+        // change the button text
         setEnabledButtonText();
+
+        // send a message to the contents.js with the new value
+        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {type:"filter", inversionEnabled: enabled});
+            console.log("Message sent");
+        });
     }
 
 });
